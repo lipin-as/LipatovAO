aa5986f4c00892de46e3bbc80f1db6eec6c7426b
lipin-as <55536636+lipin-as@users.noreply.github.com>
2023-02-03 20:04:17 +0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,20 +7,20 @@ $mail->CharSet = 'utf-8';
 $name = $_POST['user_name'];
 $phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
-$message = $_POST['user_massage']
+$message = $_POST['user_message']
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'lipatov.andrey999@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'tDAA3rsT3o$o'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('gomudusu@p33.org');     // Кому будет уходить письмо 
+$mail->setFrom('lipatov.andrey999@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('ayp22357@omeie.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
